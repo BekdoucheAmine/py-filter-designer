53988c5c823751f9f12ae09045b1547bcbb1970f
BekdoucheAmine <amineadelbekdouche@gmail.com>
2026-04-25 13:05:12 +0200

    refactor+feat: app.py file (Now it links the front and back, better UX, and taps are rendered in easy-to-copy py-code format)

--- app/app.py
+++ app/app.py
@@ -1,22 +1,36 @@
 import streamlit as st
+from back.filter import filter
+from back.spec import specs
+from back.tap import taps
 from front.main import *
+from front.plot import plotter
 
 def main():
     st.set_page_config(page_title="Filter Designer", layout="wide")
     st.title("Filter Characteristics")
     st.markdown("")
 
-    render_sidebar()
+    col1, col2, col3 = st.columns((1,3,1))
 
-    m_col1, m_col2, m_col3 = st.columns(3, vertical_alignment="top")
-    with m_col1:
+    sidebar = st.sidebar
+    
+    f = filter()
+    spec = specs(f)
+    plt = plotter(spec)
+    coef = taps(spec)
+    
+    with sidebar:
+        render_type()
         render_dm()
-    with m_col2:
-        render_options()
-    with m_col3:
-        render_graph_settings()
 
-    render_plot()
+    with col1:
+        render_options()
+    with col3:
+        render_plot_param()
+        render_taps_param()
+        render_taps(coef.get_taps())
+    with col2:
+        render_plot(plt)
 
 if __name__ == "__main__":
     main()

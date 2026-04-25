2b2ee3df10135711ff36fd68f1bdf68495e1dfeb
BekdoucheAmine <amineadelbekdouche@gmail.com>
2026-04-25 20:43:44 +0200

    feat+refactor: spec.py file (get filter parameter inside function, update filter function, get design method, get_taps refactored using these functions)

--- app/back/spec.py
+++ app/back/spec.py
@@ -10,8 +10,14 @@ class specs:
     def get_worN(self):
         return st.session_state.get("worN")
 
+    def get_dm(self):
+        """
+        """
+        return st.session_state.get("design-method")
+
     def get_fs(self):
-        dm = st.session_state.get("design-method")
+        dm = self.get_dm()
+
         if dm == "Window":
             return st.session_state.get("window-fs")
         elif dm == "Frequency Sampling":
@@ -21,12 +27,15 @@ class specs:
         elif dm == "Equiripple/Minimax":
             return st.session_state.get("remez-fs")
     
-    def get_taps(self):
-        self.filter.set_function(st.session_state.get("design-method"))
-        dm = st.session_state.get("design-method")
+    def update_filter(self):
+        self.filter.set_function(self.get_dm())
+
+    def get_params(self):
+        param = []
+        dm = self.get_dm()
         if dm == "Window":
             use_width = st.session_state.get("window-width_checkbox")
-            param = [
+            param.extend([
                 st.session_state.get("window-numtaps"),
                 st.session_state.get("window-df")["Frequencies"],
                 st.session_state.get("window-width") if use_width else None,
@@ -34,11 +43,9 @@ class specs:
                 st.session_state.get("window-pass_zero"),
                 st.session_state.get("window-scale"),
                 st.session_state.get("window-fs")
-            ]
-            self.filter.set_param(param)
-            return self.filter.get_taps()
+            ])
         elif dm == "Frequency Sampling":
-            param = [
+            param.extend([
                 st.session_state.get("freq_smp-numtaps"),
                 st.session_state.get("freq_smp-freq"),
                 st.session_state.get("freq_smp-gain"),
@@ -46,22 +53,17 @@ class specs:
                 st.session_state.get("freq_smp-window"),
                 st.session_state.get("freq_smp-antisymmetric"),
                 st.session_state.get("freq_smp-fs")
-            ]
-            self.filter.set_param(param)
-            return self.filter.get_taps()
+            ])
         elif dm == "Least Squares":
-            param = [
+            param.extend([
                 st.session_state.get("lst_sq-numtaps"),
                 st.session_state.get("lst_sq-bands"),
                 st.session_state.get("lst_sq-desired"),
                 st.session_state.get("lst_sq-weight"),
                 st.session_state.get("lst_sq-fs")
-            ]
-            self.filter.set_param(param)
-            return self.filter.get_taps()
+            ])
         elif dm == "Equiripple/Minimax":
-            param = [
-                st.session_state.get("remez-numtaps"),
+            param.extend([st.session_state.get("remez-numtaps"),
                 st.session_state.get("remez-bands"),
                 st.session_state.get("remez-desired"),
                 st.session_state.get("remez-weight"),
@@ -69,6 +71,11 @@ class specs:
                 st.session_state.get("remez-maxiter"),
                 st.session_state.get("remez-grid_density"),
                 st.session_state.get("remez-fs")
-            ]
-            self.filter.set_param(param)
-            return self.filter.get_taps()
+            ])
+        return param
+
+    def get_taps(self):
+        self.update_filter()
+        param = self.get_params()
+        self.filter.set_param(param)
+        return self.filter.get_taps()

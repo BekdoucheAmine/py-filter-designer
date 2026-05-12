3711ba775e791ea71d651c2886e31b38fe27aaf2
BekdoucheAmine <amineadelbekdouche@gmail.com>
2026-05-12 12:04:51 +0200

    feat: added all types of windows support and compatibility with front

--- app/back/spec.py
+++ app/back/spec.py
@@ -30,20 +30,65 @@ class specs:
     def update_filter(self):
         self.filter.set_function(self.get_dm())
 
+    def __get_window_window_param(self):
+        """
+
+        """
+        window = st.session_state.get("window-window")
+        if window == "chebwin":
+            return (window, st.session_state.get("window-window_chebwin_attenuation"))
+        elif window == "dpss":
+            return (window, st.session_state.get("window-window_dpss_nw"))
+        elif window == "exponential":
+            return (window,
+                    None,
+                    st.session_state.get("window-window_exponential_tau"))
+        elif window == "gaussian":
+            return (window, st.session_state.get("window-window_gaussian_std"))
+        elif window == "general cosine":
+            return (window,
+                    st.session_state.get("window-window_general_cosine_coef_df")["Weighted Coefficients"])
+        elif window == "general gaussian":
+            return (window,
+                    st.session_state.get("window-window_general_gaussian_p"),
+                    st.session_state.get("window-window_general_gaussian_std"))
+        elif window == "general hamming":
+            return (window, st.session_state.get("window-window_general_hamming_alpha"))
+        elif window in ["kaiser bessel derived", "kaiser"]:
+            return (window, st.session_state.get("window-window_kaiser_beta"))
+        elif window == "taylor":
+            return (window,
+                    st.session_state.get("window-window_taylor_nbar"),
+                    st.session_state.get("window-window_taylor_sll"),
+                    st.session_state.get("window-window_taylor_norm"))
+        elif window == "tukey":
+            return (window, st.session_state.get("window-window_tukey_alpha"))
+        else:
+            return window
+
+    def _get_window_params(self):
+        """
+
+        """
+        use_width = st.session_state.get("window-width_checkbox")
+        
+        return [
+            st.session_state.get("window-numtaps"),
+            st.session_state.get("window-df")["Frequencies"],
+            st.session_state.get("window-width") if use_width else None,
+            self.__get_window_window_param(),
+            st.session_state.get("window-pass_zero"),
+            st.session_state.get("window-scale"),
+            st.session_state.get("window-fs")]
+
     def get_params(self):
-        param = []
+        """
+
+        """
+        param = [] # TODO: remove this and replace with direct return
         dm = self.get_dm()
         if dm == "Window":
-            use_width = st.session_state.get("window-width_checkbox")
-            param.extend([
-                st.session_state.get("window-numtaps"),
-                st.session_state.get("window-df")["Frequencies"],
-                st.session_state.get("window-width") if use_width else None,
-                st.session_state.get("window-window"),
-                st.session_state.get("window-pass_zero"),
-                st.session_state.get("window-scale"),
-                st.session_state.get("window-fs")
-            ])
+            return self._get_window_params()
         elif dm == "Frequency Sampling":
             param.extend([
                 st.session_state.get("freq_smp-numtaps"),
@@ -71,10 +116,12 @@ class specs:
                 st.session_state.get("remez-maxiter"),
                 st.session_state.get("remez-grid_density"),
                 st.session_state.get("remez-fs")
-            ])
-        return param
+            ]) 
 
     def get_taps(self):
+        """
+
+        """
         self.update_filter()
         param = self.get_params()
         self.filter.set_param(param)

49240091e8b735fede6769b2d22025777b42e18d
BekdoucheAmine <amineadelbekdouche@gmail.com>
2026-05-12 12:03:50 +0200

    feat & fix: local firwin implementation that fixes general_cosine usage

--- app/back/filter.py
+++ app/back/filter.py
@@ -1,4 +1,8 @@
-from scipy.signal import firwin, firwin2, firls, remez
+from scipy.signal import firwin, firwin2, firls, remez, kaiser_atten, kaiser_beta
+from scipy.signal._arraytools import _validate_fs
+from scipy.signal.windows import get_window, general_cosine
+from scipy._lib._array_api import array_namespace, xp_size, xp_default_dtype
+import scipy._lib.array_api_extra as xpx
 
 class filter:
     """
@@ -14,7 +18,7 @@ class filter:
         """
         self.f_name = f_name
         if f_name == "Window":
-            self.f = firwin
+            self.f = self._firwin
         elif f_name == "Frequency Sampling":
             self.f = firwin2
         elif f_name == "Least Squares":
@@ -64,7 +68,111 @@ class filter:
                 "maxiter": param[5],
                 "grid_density": param[6],
                 "fs": param[7]
-            }
+            }   
+    def _firwin(self, numtaps, cutoff, *, width=None, window='hamming', pass_zero=True, scale=True, fs=None):
+        """
+            this is a local copy of firwin that fixes the issue of general cosine
+        """
+        # NB: scipy's version of array_namespace returns `np_compat` for int or floats
+        xp = array_namespace(cutoff)
+
+        # The major enhancements to this function added in November 2010 were
+        # developed by Tom Krauss (see ticket #902).
+        fs = _validate_fs(fs, allow_none=True)
+        fs = 2 if fs is None else fs
+
+        nyq = 0.5 * fs
+
+        cutoff = xp.asarray(cutoff, dtype=xp_default_dtype(xp))
+        cutoff = xpx.atleast_nd(cutoff, ndim=1, xp=xp) / float(nyq)
+
+        # Check for invalid input.
+        if cutoff.ndim > 1:
+            raise ValueError("The cutoff argument must be at most "
+                            "one-dimensional.")
+        if xp_size(cutoff) == 0:
+            raise ValueError("At least one cutoff frequency must be given.")
+        if xp.min(cutoff) <= 0 or xp.max(cutoff) >= 1:
+            raise ValueError("Invalid cutoff frequency: frequencies must be "
+                            "greater than 0 and less than fs/2.")
+        if xp.any(cutoff[1:] - cutoff[:-1] <= 0):
+            raise ValueError("Invalid cutoff frequencies: the frequencies "
+                            "must be strictly increasing.")
+
+        if width is not None:
+            # A width was given.  Find the beta parameter of the Kaiser window
+            # and set `window`.  This overrides the value of `window` passed in.
+            atten = kaiser_atten(numtaps, float(width) / nyq)
+            beta = kaiser_beta(atten)
+            window = ('kaiser', beta)
+
+        if pass_zero in ('bandstop', 'lowpass'):
+            if pass_zero == 'lowpass':
+                if xp_size(cutoff) != 1:
+                    raise ValueError('cutoff must have one element if '
+                                    f'pass_zero=="lowpass", got {cutoff.shape}')
+            elif xp_size(cutoff) <= 1:
+                raise ValueError('cutoff must have at least two elements if '
+                                f'pass_zero=="bandstop", got {cutoff.shape}')
+            pass_zero = True
+        elif pass_zero in ('bandpass', 'highpass'):
+            if pass_zero == 'highpass':
+                if xp_size(cutoff) != 1:
+                    raise ValueError('cutoff must have one element if '
+                                    f'pass_zero=="highpass", got {cutoff.shape}')
+            elif xp_size(cutoff) <= 1:
+                raise ValueError('cutoff must have at least two elements if '
+                                f'pass_zero=="bandpass", got {cutoff.shape}')
+            pass_zero = False
+        elif not (pass_zero is True or pass_zero is False):
+            raise ValueError(f"Parameter {pass_zero=} not in (True, False, 'bandpass', " +
+                            "'lowpass', 'highpass', 'bandstop')")
+
+        pass_nyquist = (xp_size(cutoff) % 2 == 0) == pass_zero
+        if pass_nyquist and numtaps % 2 == 0:
+            raise ValueError("A filter with an even number of coefficients must "
+                            "have zero response at the Nyquist frequency.")
+
+        # Insert 0 and/or 1 at the ends of cutoff so that the length of cutoff
+        # is even, and each pair in cutoff corresponds to passband.
+        cutoff = xp.concat((xp.zeros(int(pass_zero)), cutoff, xp.ones(int(pass_nyquist))))
+
+
+        # `bands` is a 2-D array; each row gives the left and right edges of
+        # a passband.
+        bands = xp.reshape(cutoff, (-1, 2))
+
+        # Build up the coefficients.
+        alpha = 0.5 * (numtaps - 1)
+        m = xp.arange(0, numtaps, dtype=cutoff.dtype) - alpha
+        h = 0
+        for j in range(bands.shape[0]):
+            left, right = bands[j, 0], bands[j, 1]
+            h += right * xpx.sinc(right * m, xp=xp)
+            h -= left * xpx.sinc(left * m, xp=xp)
+
+        # Get and apply the window function.
+        if window[0] == "general cosine":
+            win = general_cosine(numtaps, window[1], sym=True)
+        else:
+            win = get_window(window, numtaps, fftbins=False, xp=xp)
+        h *= win
+
+        # Now handle scaling if desired.
+        if scale:
+            # Get the first passband.
+            left, right = bands[0, ...]
+            if left == 0:
+                scale_frequency = 0.0
+            elif right == 1:
+                scale_frequency = 1.0
+            else:
+                scale_frequency = 0.5 * (left + right)
+            c = xp.cos(xp.pi * m * scale_frequency)
+            s = xp.sum(h * c)
+            h /= s
+
+        return h
     def get_taps(self):
         """
         """
